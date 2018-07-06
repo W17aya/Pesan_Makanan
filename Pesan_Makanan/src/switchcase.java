e4cd6564bce93f9c70201174dab12a921b2abbf2
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-07-07 00:46:56 +0800

    Mark_6

--- Pesan_Makanan/src/switchcase.java
+++ Pesan_Makanan/src/switchcase.java
@@ -6,25 +6,27 @@ public class switchcase
     {
 //        inisialisasi java library scanner
         Scanner scan = new Scanner(System.in);
-        Scanner scan2 = new Scanner (System.in);
+      
         
 //        menampilkan menu makanan
         System.out.println("MENU MAKANAN : ");
         System.out.println("___________________________________________________________________");
         System.out.println("No Nama             Harga");
-        System.out.println("1. Batagor          Rp 3000,00");
-        System.out.println("2. Kupat Tahu       Rp 5000,00");
-        System.out.println("3. Lontong Kari     Rp 4000,00");
+        System.out.println("1. Batagor          Rp 10000,00");
+        System.out.println("2. Kupat Tahu       Rp 10000,00");
+        System.out.println("3. Lontong Kari     Rp 15000,00");
+        System.out.println("3. Lontong Kari     Rp 10000,00");
         System.out.println("");
 
 //        inisialisasi pembayaran awal
 
-         int jumlah= 0;
+       
         int harga = 0 ;
+        int jumlah;
         
         
 //        inisialisasi harga ketiga menu
-        int h1 = 3000, h2 = 5000, h3 = 4000;
+        int h1 = 10000, h2 = 10000, h3 = 15000, h4= 10000;
         String menu;
 
         for (String i = "Y"; i.equals("Y")||i.equals("y"); )
@@ -34,31 +36,28 @@ public class switchcase
         System.out.print("Masukkan Nomor Pesanan : ");
         int inNomor = scan.nextInt();
         System.out.println("___________________________________________________________________");
-//        pengguna memasukkan nomor pesanan
-        System.out.print("Masukkan jumlah : ");
-        int injumlah = scan2.nextInt();
-        System.out.println("___________________________________________________________________");
+
 
         if (inNomor == 1)
         {
 //            jika nomor satu, maka menu yang dipilih adalah batagor, dan harganya adalah h1, yang merupakan harga menu pertama
             menu = " Batagor";
             System.out.println("Pilihan anda nomor " +inNomor + menu);
-            harga =jumlah*h1;
+            harga =harga+h1;
         }
             else if (inNomor == 2)
             {
 //            jika nomor dua, maka menu yang dipilih adalah kupat tahu, dan harganya adalah h2, yang merupakan harga menu kedua
                 menu = " Kupat Tahu";
                 System.out.println("Pilihan anda nomor " +inNomor + menu);
-                harga = jumlah*h2;
+                harga = harga+h2;
             }
                 else if (inNomor == 3)
                 {
 //            jika nomor tiga, maka menu yang dipilih adalah lontong kari, dan harganya adalah h3, yang merupakan harga menu ketiga
                     menu = " Lontong Kari";
                     System.out.println("Pilihan anda nomor " +inNomor + menu);
-                    harga = jumlah*h3;
+                    harga = harga+h3;
                 }
                      else
 //            jika selain nomor satu sampai tiga, maka akan ditampilkan "Nomor yang dipilih tidak ada di menu."
