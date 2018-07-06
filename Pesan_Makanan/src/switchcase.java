3df3a96fd1b4f775bc097736f4337356c04564de
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-07-07 00:30:05 +0800

    Mark_5
    Perkalian masih error

--- Pesan_Makanan/src/switchcase.java
+++ Pesan_Makanan/src/switchcase.java
@@ -6,6 +6,8 @@ public class switchcase
     {
 //        inisialisasi java library scanner
         Scanner scan = new Scanner(System.in);
+        Scanner scan2 = new Scanner (System.in);
+        
 //        menampilkan menu makanan
         System.out.println("MENU MAKANAN : ");
         System.out.println("___________________________________________________________________");
@@ -16,7 +18,11 @@ public class switchcase
         System.out.println("");
 
 //        inisialisasi pembayaran awal
-        int harga = 0;
+
+         int jumlah= 0;
+        int harga = 0 ;
+        
+        
 //        inisialisasi harga ketiga menu
         int h1 = 3000, h2 = 5000, h3 = 4000;
         String menu;
@@ -28,27 +34,31 @@ public class switchcase
         System.out.print("Masukkan Nomor Pesanan : ");
         int inNomor = scan.nextInt();
         System.out.println("___________________________________________________________________");
+//        pengguna memasukkan nomor pesanan
+        System.out.print("Masukkan jumlah : ");
+        int injumlah = scan2.nextInt();
+        System.out.println("___________________________________________________________________");
 
         if (inNomor == 1)
         {
 //            jika nomor satu, maka menu yang dipilih adalah batagor, dan harganya adalah h1, yang merupakan harga menu pertama
             menu = " Batagor";
             System.out.println("Pilihan anda nomor " +inNomor + menu);
-            harga = harga + h1;
+            harga =jumlah*h1;
         }
             else if (inNomor == 2)
             {
 //            jika nomor dua, maka menu yang dipilih adalah kupat tahu, dan harganya adalah h2, yang merupakan harga menu kedua
                 menu = " Kupat Tahu";
                 System.out.println("Pilihan anda nomor " +inNomor + menu);
-                harga = harga + h2;
+                harga = jumlah*h2;
             }
                 else if (inNomor == 3)
                 {
 //            jika nomor tiga, maka menu yang dipilih adalah lontong kari, dan harganya adalah h3, yang merupakan harga menu ketiga
                     menu = " Lontong Kari";
                     System.out.println("Pilihan anda nomor " +inNomor + menu);
-                    harga = harga + h3;
+                    harga = jumlah*h3;
                 }
                      else
 //            jika selain nomor satu sampai tiga, maka akan ditampilkan "Nomor yang dipilih tidak ada di menu."
@@ -62,7 +72,7 @@ public class switchcase
 
         }
         System.out.println("___________________________________________________________________");
-        System.out.println("Total pembayaran sebesar " +harga+".");
+      System.out.println("Total Pembayaran Sebesar "+harga+".");
         System.out.println("Terimakasih atas kunjungannya.");
     }
 }
